535314574ee7e42ee60df8a285997a9b08d86eca
AK <101114294+AKAu0124@users.noreply.github.com>
2022-06-25 23:55:58 +0800

    master

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-document.addEventListener("click", e => {
-    const isDropDownButton = e.target.matches("[data-dropdown-button]")
-    if (!isDropDownButton && e.target.closest('[data-dropdown]') != null) return
+/*document.addEventListener("click", e => {
+    const isDropDownButton = e.target.matches("[rpt-btn]")
+    if (!isDropDownButton && e.target.closest('[rpt-show]') != null) return
 
     let currentDropdown
     if (isDropDownButton) {
-        currentDropdown = e.target.closest('[data-dropdown]')
+        currentDropdown = e.target.closest('[rpt-show]')
         currentDropdown.classList.toggle('active')
     }
 
-    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+    document.querySelectorAll("[rpt-show].active").forEach(dropdown => {
         if (dropdown === currentDropdown) return
         dropdown.classList.remove('active')
     })
-})
+})*/
+
+
+function rptOpen() {
+    const show = document.querySelector('.rpt-show');
+    show.classList.toggle('show');
+    console.log()
+}
+
+function empOpen() {
+    const show = document.querySelector('.emp-show');
+    show.classList.toggle('show');
+    console.log()
+}
